cedc861b3c0bc4282e6fbb0528ebbdbfc8e206f3
kyuumm <kyuuyxx@outlook.com>
2025-05-08 22:31:35 +0800

    添加项目文件。

---
+++ Plane/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include"tools.hpp"
+#include<easyx.h>
+int main() {
+	initgraph(480, 852);
+
+	IMAGE img_bk;
+
+	loadimage(&img_bk, "asset/image/background.png");
+	putimage(0, 0, &img_bk);
+	getchar();
+	return 0;
+}
